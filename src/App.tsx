24b697f5cbceee488626cbee79405dadb472b710
바림 <balim@balim-ui-iMac.local>
2022-01-25 18:26:40 +0900

    new Start

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+const App = () => {
+  return (
+    <div className="App" style={{ width: '100px', height: '500px' }}>
+      Hello Thank you
+    </div>
+  );
+};
+
+export default App;
