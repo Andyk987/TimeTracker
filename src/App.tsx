5cb0fb3a334cdb87a9492f4d6e53150332796579
바림 <balim@balim-ui-iMac.local>
2022-01-26 22:52:57 +0900

    start styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import * as React from "react";
+import styled from "styled-components";
+
+const AppStyled = styled.div`
+  width: 375px;
+  height: 600px;
+`;
 
 const App = () => {
   return (
-    <div className="App" style={{ width: '100px', height: '500px' }}>
-      Hello Thank you
-    </div>
+    <AppStyled>
+      <button>button</button>
+    </AppStyled>
   );
 };
 
--- src/popup.tsx
+++ src/popup.tsx
@@ -2,6 +2,13 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 import App from "./App";
+import GlobalStyles from "./styles/global-styles";
 
 var mountNode = document.getElementById("popup");
-ReactDOM.render(<App />, mountNode);
+ReactDOM.render(
+  <>
+    <GlobalStyles />
+    <App />
+  </>,
+  mountNode
+);
